9899174abb2ff359edc0726a062d9654b0d42402
Luke Dojan <dojanl@wwu.edu>
2024-06-18 13:09:21 -0700

    git ignore & scripts

---
+++ scripts/fashion-mnist.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/zalandoresearch/fashion-mnist/raw/master/data/fashion/t10k-images-idx3-ubyte.gz -P data/
+wget https://github.com/zalandoresearch/fashion-mnist/raw/master/data/fashion/t10k-labels-idx1-ubyte.gz -P data/
+wget https://github.com/zalandoresearch/fashion-mnist/raw/master/data/fashion/train-images-idx3-ubyte.gz -P data/
+wget https://github.com/zalandoresearch/fashion-mnist/raw/master/data/fashion/train-labels-idx1-ubyte.gz -P data/
